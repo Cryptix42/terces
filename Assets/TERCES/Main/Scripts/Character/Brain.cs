ee3085c3ec10e552df1cf3dc2f31a4efba419f0f
Aiden Sognatore <aiden@firedreamer.com>
2020-03-10 13:56:50 +0530

    SHOVED THE HEAD THROUGH A WOODDEN PLANK

---
+++ Assets/TERCES/Main/Scripts/Character/Brain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    public Transform EyeLocation;
+    public float AngleH, AngleV;
+    public GameObject[] POI_InMemory;
+    public GameObject[] CharactersInMemory;
+    public string[] OffensiveTags;
+    public string[] FriendlyTags;
+    RaycastHit hit;
+
+    Vector3 EyeH, EyeV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/TERCES/Main/Scripts/Character/Eye.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+
+    public float maxRadius;
+    public int i = 0;
+    public float stepValue;
+    RaycastHit hit;
+    private float curRad = 0.0f;
+    void FixedUpdate()
+    {
+        if(curRad<maxRadius)
+        {
+            if (Physics.SphereCast(transform.position, curRad, transform.localRotation.eulerAngles, out hit))
+            {
+                print("Hit!");
+            }
+        }
+        curRad += stepValue;
+        if(curRad>maxRadius)
+        {
+            curRad = 0.0f;
+        }
+        i++;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, curRad);
+    }
+    //
+}
